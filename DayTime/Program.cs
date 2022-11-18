b310973811cbfef73c02c9db2b6759cbfa3819ee
Студент 3 < 3@DESKTOP-PS97MJ6>
2022-11-18 11:03:00 +0300

    Добавьте файлы проекта.

---
+++ DayTime/Program.cs
@@ -0,0 +1,53 @@
+﻿
+void Menu()
+{
+    Console.WriteLine();
+    Console.WriteLine($"Введите товар который вы хотите купить");
+    Console.WriteLine($"1) Телевизор");
+    Console.WriteLine($"2) холодильник");
+    Console.WriteLine($"3) микровалновка");
+    Console.WriteLine($"4) кофеварка");
+    Console.WriteLine();
+}
+void GetMenu()
+{
+    DateTime date = DateTime.Now;
+    while (true)
+    {
+        Menu();
+        string enter = Console.ReadLine();
+        bool check = int.TryParse(enter, out int result);
+        if (check == true)
+        {
+            if (result == 1)
+            {
+                Console.WriteLine($"Ваш товар будет доставлен через неделю");
+                var newDate = date.AddDays(7);
+                Console.WriteLine(newDate.ToShortDateString());
+            }
+            else if (result == 2)
+            {
+                Console.WriteLine($"Ваш товар будет доставлен через неделю");
+                var newDate = date.AddDays(7);
+                Console.WriteLine(newDate.ToShortDateString());
+            }
+            else if (result == 3)
+            {
+                Console.WriteLine($"Ваш товар будет доставлен через неделю");
+                var newDate = date.AddDays(7);
+                Console.WriteLine(newDate.ToShortDateString());
+            }
+            else if (result == 4)
+            {
+                Console.WriteLine($"Ваш товар будет доставлен через неделю");
+                var newDate = date.AddDays(7);
+                Console.WriteLine(newDate.ToShortDateString());
+            }
+            else { Console.WriteLine($"Введите пункт из меню"); }
+        }
+        else
+        {
+            Console.WriteLine($"Введите пункт из меню");
+        }
+    }
+}
